e71a02f05abf24af441e65905de4fd733d96d7f7
Hans Dulimarta <hans.dulimarta@gmail.com>
2025-08-11 18:25:22 -0400

    Add a second query to pull all launches

--- spacex-graphql-compose/app/src/main/java/edu/gvsu/cis357/spacex_graphql/MainActivity.kt
+++ spacex-graphql-compose/app/src/main/java/edu/gvsu/cis357/spacex_graphql/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
@@ -17,6 +24,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.gvsu.cis357.spacex_graphql.ui.theme.SpaceXGraphQLComposeTheme
@@ -42,6 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(modifier: Modifier = Modifier, vm : MainViewModel) {
     val name =  vm.ceoName.collectAsState()
+    val launches = vm.launches.collectAsState()
     val loading = rememberSaveable { mutableStateOf(false) }
     Column(modifier = modifier.padding(all = 8.dp).fillMaxSize()) {
         if(name.value != null) {
@@ -54,15 +63,41 @@ fun Greeting(modifier: Modifier = Modifier, vm : MainViewModel) {
             LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
 
-        Button(onClick = {
-            loading.value = true
-            vm.sendQuery()
-        }) {
-            Text("Get")
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround) {
+            Button(onClick = {
+                loading.value = true
+                vm.sendCompanyQuery()
+            }) {
+                Text("Company")
+            }
+            Button(onClick = {
+                loading.value = true
+                vm.sendMissionQuery()
+            }) {
+                Text("Missions")
+            }
+        }
+        if (launches.value.size > 0) {
+            loading.value = false
+            LazyColumn {
+                itemsIndexed(launches.value) { idx, item ->
+                    LaunchItem(idx, item)
+                }
+            }
         }
     }
 }
 
+@Composable
+private fun LaunchItem(pos: Int, z: AllLaunchesQuery.Launch) {
+    Column(modifier = Modifier.fillMaxWidth()
+        .padding(vertical = 2.dp)
+        .border(1.dp, Color.Black).padding(all = 4.dp)) {
+        Text(z.mission_name!!)
+        Text(z.launch_date_utc.toString())
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- spacex-graphql-compose/app/src/main/java/edu/gvsu/cis357/spacex_graphql/MainViewModel.kt
+++ spacex-graphql-compose/app/src/main/java/edu/gvsu/cis357/spacex_graphql/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.network.okHttpClient
+import edu.gvsu.cis357.spacex_graphql.adapter.AllLaunchesQuery_ResponseAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,11 +20,13 @@ class MainViewModel: ViewModel() {
         .build()
     private val _ceoName = MutableStateFlow<String?>(null)
     val ceoName = _ceoName.asStateFlow()
-
+    private val _launhces = MutableStateFlow<List<AllLaunchesQuery.Launch>>(emptyList())
+    val launches = _launhces.asStateFlow()
     init {
         println("Init block is invoked")
     }
-    fun sendQuery() {
+
+    fun sendCompanyQuery() {
         _ceoName.value = null
         viewModelScope.launch(Dispatchers.IO) {
             val q = CEOQuery()
@@ -32,6 +35,14 @@ class MainViewModel: ViewModel() {
             _ceoName.update {
                 result.data?.company?.ceo
             }
+        }
+    }
+
+    fun sendMissionQuery() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val q = AllLaunchesQuery()
+            val result = apolloClient.query(q).execute()
+            _launhces.value = result.data?.launches?.mapNotNull { it } ?: emptyList()
         }
     }
 }
